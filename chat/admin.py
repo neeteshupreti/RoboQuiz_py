9b3a9e8cfcd6c2c13073b16dd911879415438aba
Jiya Fatma <jiyafatma9@gmail.com>
2026-01-16 20:38:19 +0545

    Fix merge conflict in chat/admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 from django.contrib import admin
+from .models import ChatMessage
 
-# Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> 3d5c8dafa1c7522f9648891421193305a5191b1e
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('user_message', 'bot_response', 'timestamp')
