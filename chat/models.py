2b1251f633cf676a61ab6cf95aced0b0463c1e1d
Jiya Fatma <jiyafatma9@gmail.com>
2026-01-16 20:36:03 +0545

    Fix merge conflict in chat/models.py

--- chat/models.py
+++ chat/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 
 class ChatMessage(models.Model):
@@ -8,14 +7,3 @@ class ChatMessage(models.Model):
 
     def __str__(self):
         return f"{self.user_message[:30]}..."
-=======
-from django.db import models
-
-class ChatMessage(models.Model):
-    user_message = models.TextField()
-    bot_response = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user_message[:30]}..."
->>>>>>> 3d5c8dafa1c7522f9648891421193305a5191b1e
